db6a727cfe469ed9828f2390d28cd32478a7126f
Malinnik <ratanov.maxim@gmail.com>
2024-07-07 02:33:13 +0500

    Улучшенная версия сабмишинов

--- frontend/src/components/DownloadSubmission.tsx
+++ frontend/src/components/DownloadSubmission.tsx
@@ -41,7 +41,7 @@ const DownloadSubmission = ({id, filename, ready}: {id: number, filename: string
       // className="rounded-lg text-blue-500 hover:text-white hover:bg-blue-500 border border-blue-500"
         <button className={ready ? 
           "rounded-lg text-blue-500 hover:text-white hover:bg-blue-500 border border-blue-500" : 
-          " bg-slate-400  text-white font-semibold border rounded"} onClick={handleClick}>
+          " bg-white  text-gray-400 font-semibold border rounded"} onClick={handleClick}>
             <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8"> 
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /> 
             </svg>
--- frontend/src/components/SubmissionsTable.tsx
+++ frontend/src/components/SubmissionsTable.tsx
@@ -19,24 +19,25 @@ const SubmissionTable = ({data}: {data: Submission[]}) => {
     }
 
     return (
-        <div className="bg-white w-full md:col-span-3 relative h-[90vh] m-auto p-4 border rounded-lg">
-            <table className="w-full">
-                <thead>
+        <div className="bg-white w-full md:col-span-3 relative h-[90vh] m-auto p-4 border rounded-lg overflow-y-scroll">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
                     <tr>
-                        <td>ID</td>
-                        <td>название</td>
-                        <td>Дата создания</td>
-                        <td>Статус</td>
+                        <td className="px-6 py-3 text-center">ID</td>
+                        <td className="px-6 py-3 text-center">название</td>
+                        <td className="px-6 py-3 text-center">Дата создания</td>
+                        <td className="px-6 py-3 text-center">Статус</td>
+                        <td className="px-6 py-3 text-center">Скачать</td>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody >
                     {data.map((submission, index) => (
-                    <tr key={index} className="">
-                        <td>{submission.id}</td>
-                        <td>{submission.path}</td>
-                        <td>{submission.created_at}</td>
-                        <td> <Dot status={submission.status}/></td>       
-                        <td><DownloadSubmission id={submission.id} filename={submission.path} ready={isReady(submission.status)}/></td>         
+                    <tr key={index} className="bg-white border-b hover:bg-gray-200 ">
+                        <td className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap">{submission.id}</td>
+                        <td className="px-6 py-4 text-center">{submission.path}</td>
+                        <td className="px-6 py-4 text-center">{submission.created_at}</td>
+                        <td className="px-6 py-4 grid place-items-center"> <Dot status={submission.status}/></td>       
+                        <td className="px-6 py-4 text-center"><DownloadSubmission id={submission.id} filename={submission.path} ready={isReady(submission.status)}/></td>         
                     </tr>
                     ))}
                 </tbody>
